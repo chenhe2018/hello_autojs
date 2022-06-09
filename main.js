ba9dad756d079c30cae6c37f0d24aca6e9332aa8
chenhe2018 <watch_ch@163.com>
2022-06-09 10:46:50 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+toast('加油');
+
+auto()
+
+console.show()
+toastLog(app.versionCode)
+toastLog(app.versionName)
+toastLog(app.autojs.versionCode)
+toastLog(app.autojs.versionName)
+app.launchApp("抖音")
+
+app.startActivity("console")
+
+
+app.startActivity("settings")
